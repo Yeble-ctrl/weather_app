f7f365bc5819dfe023e011db8cc12b2dca332ec6
Yewo Manombo <ymanombo@gmail.com>
2025-03-13 18:52:58 +0200

    1. Included weather_code as part of the values of the current variable in the API call parameters 2. Included a VMO data description varible for interpreting the value of the weather codes. 3. Added a method for changing the date and time format from a given ISO formart to a more readable formart

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from datetime import datetime
+from datetime import datetime, date
 from requests import get
 from requests import exceptions
+from weather_app.vmo_description_data import VMO_DESCRIPTION_DATA
 
 def index(request):
     """ View function for returning the home/index page"""
@@ -14,8 +15,11 @@ def index(request):
             data["hourly"]["temperature_2m"], 
             data["hourly"]["cloud_cover_low"]
         )
-        date_time_data = getDateAndTime()
+        date_time_data = getCurrDateAndTime()
         loc_data = getLocation()
+
+        print(current_data)
+
         context = {"current": current_data, "daily": daily_data, 
         "hourly": hourly_data, "date_time": date_time_data, "location": loc_data}
 
@@ -27,11 +31,16 @@ def index(request):
 def weekly_forecast(request):
     """View function to get weeky weather forecast"""
     try:
-        data = getWeatherData()
+        data = getWeatherData()["daily"]
+
+        # Change the date from isoformat
+        for i in range(0, len(data["time"])):
+            data["time"][i] = changeDateFormat(data["time"][i])
+
         weekly_data = zip(
-            data["daily"]["time"],
-            data["daily"]["temperature_2m_max"],
-            data["daily"]["temperature_2m_min"]
+            data["time"],
+            data["temperature_2m_max"],
+            data["temperature_2m_min"]
         )
         context = {"weekly_data": weekly_data}
 
@@ -54,7 +63,7 @@ def getWeatherData():
             "longitude": lon,
             "timezone": "auto",
             "forecast_hours": 12,
-            "current": ["is_day", "temperature_2m", "cloud_cover_low"],
+            "current": ["is_day", "temperature_2m", "cloud_cover_low", "weather_code"],
             "daily": ["temperature_2m_max", "temperature_2m_min", "sunset", "sunrise"],
             "hourly": ["temperature_2m", "cloud_cover_low"]
         })
@@ -62,8 +71,20 @@ def getWeatherData():
     wtr_data_dic = wtr_data.json() # Convert the Json response to a dictionary
     wtr_data_dic["hourly"]["temperature_2m"][0] = wtr_data_dic["current"]["temperature_2m"] # Update the current hour temp with the current real time temp
     wtr_data_dic["hourly"]["cloud_cover_low"][0] = wtr_data_dic["current"]["cloud_cover_low"] # Update the curremt hour cloud cover %ge with the current real time cloud cover %ge
-    wtr_data_dic["hourly"]["time"][0] = getDateAndTime()["time"] # Update the current hour value with a more accurate value
+    wtr_data_dic["hourly"]["time"][0] = getCurrDateAndTime()["time"] # Update the current hour value with a more accurate value
+    wtr_data_dic["current"].update({"weather_desc": VMO_DESCRIPTION_DATA[str(wtr_data_dic["current"]["weather_code"])]})
     return wtr_data_dic
+
+def changeDateFormat(isoformat_date):
+    """Method for changing the isodate format to a more readable one"""
+    new_date = date.fromisoformat(isoformat_date)
+    date_dic = {
+        "week_day": new_date.strftime('%A'),
+        "day": new_date.strftime('%d'),
+        "month": new_date.strftime('%B'),
+        "year": new_date.strftime('%Y'),
+    }
+    return date_dic
 
 def getLocation():
     """Functin for getting the requested location"""
@@ -72,8 +93,8 @@ def getLocation():
     loc_dic = location.json()
     return loc_dic
 
-def getDateAndTime():
-    """ Function for getting the current time and date"""
+def getCurrDateAndTime():
+    """Function for getting the current time and date"""
     curr_date = datetime.today()
     date_dic = {
         "week_day": curr_date.strftime('%A'),
@@ -82,5 +103,4 @@ def getDateAndTime():
         "year": curr_date.strftime('%Y'),
         "time": curr_date.isoformat(timespec='minutes')
     }
-
     return date_dic
