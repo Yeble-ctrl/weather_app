769c528d8f3f9f25ddd25c23c310940cc31fd8a1
Yewo Manombo <ymanombo@gmail.com>
2025-03-24 16:22:59 +0200

    Added a comment model for viewers who choose to leave a comment

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    name = models.CharField(default="Anonymous", max_length=200)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
